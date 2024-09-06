b92dc8ea8588f03afb904ec10f0adfeadc90041b
kobenguyent <7845001+kobenguyent@users.noreply.github.com>
2024-09-06 14:43:03 +0200

    Update Visual_Regression_test.ts

--- test/Visual_Regression_test.ts
+++ test/Visual_Regression_test.ts
@@ -1,7 +1,7 @@
 Feature('Visual Regression');
 
 Scenario('home page visual appearance upload screenshot', async ({ I }) => {
-  I.amOnPage('?diff1');
+  I.amOnPage('?diff2');
   const res = await I.takeScreenshot('home-page');
 });
 
